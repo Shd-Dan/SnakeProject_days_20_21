3d0b84574cfe3a073540a4d97b7d41e9bfa1f2df
Shd-Dan <shd.cs.ch@gmail.com>
2023-07-16 20:08:05 +0600

    commit

--- scoreboard.py
+++ scoreboard.py
@@ -9,6 +9,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        # attribute added in day24 lesson
+        self.high_score = 0
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -16,13 +18,17 @@ class Scoreboard(Turtle):
         self.hideturtle()
 
     def update_scoreboard(self):
-        self.write(arg=f"Scoreboard: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(arg=f"Scoreboard: {self.score} High score {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(arg="GAME OVER", align=ALIGNMENT, font=FONT)
+    # day24
+    def reset_score(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
+
 
     def counter(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
